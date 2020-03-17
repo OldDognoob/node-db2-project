1f9e23bec350250ef49c10d7c8986d3601a558c5
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-03-17 16:36:50 +0200

    set up index-file

--- data/config.js
+++ data/config.js
@@ -1,8 +1,8 @@
 //knex lib and the knexfile
 const knex =require('knex')
-const config =require('../knexfile')
+const configs =require('../knexfile')
 
 //the swiss army knife to be able to interact with the db
 //is instantiated by invoking knex passing in a specific configuration
 //(we only have "development")
-module.exports =knex(config.development)
+module.exports =knex(configs.development)
---
+++ data/migrations/20200317161244_createCarTable.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable("cars",table=>{
+      table.increment();
+      table.text("VIN",128).unique().notNullable();
+      table.text("make").notNullable();
+      table.text("model").notNullable();
+      table.integer("mileage").notNullable();
+      table.text("transmission type");
+      table.boolean("clean");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("cars");
+};
