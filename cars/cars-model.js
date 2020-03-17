e69dc6f38d3f7a610c3a2081c05c55e6df6e4c7e
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-03-17 17:50:34 +0200

    set up cars-model-file

--- cars/cars-model.js
+++ cars/cars-model.js
@@ -1,5 +1,31 @@
 //import database config
-const db =require("../data/config");
+const db = require("../data/config");
 
 //DB helpers
-
+function getAllCars() {
+  return db("cars");
+}
+function getCarById(id) {
+  return db("cars").where({ id });
+}
+function insertNewCar({ VIN, make, model, mileage, transmissionType, clean }) {
+  return db("cars").insert({
+    VIN,
+    make,
+    model,
+    mileage,
+    transmissionType,
+    clean
+  });
+}
+function updateCar({ id, VIN, make, model, mileage, transmissionType, clean }) {
+  return db("cars")
+    .where({ id })
+    .update({ VIN, make, model, mileage, transmissionType, clean });
+}
+function deleteCar(id) {
+  return db("cars")
+    .where({ id })
+    .del();
+}
+module.exports = { getAllCars, getCarById, insertNewCar, updateCar, deleteCar };
