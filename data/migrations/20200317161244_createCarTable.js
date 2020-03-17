a77a0dc396c1507b25e3921fd2fe366212378207
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-03-17 17:23:50 +0200

    database-created

--- data/migrations/20200317161244_createCarTable.js
+++ data/migrations/20200317161244_createCarTable.js
@@ -1,13 +1,15 @@
-
 exports.up = function(knex) {
-  return knex.schema.createTable("cars",table=>{
-      table.increment();
-      table.text("VIN",128).unique().notNullable();
-      table.text("make").notNullable();
-      table.text("model").notNullable();
-      table.integer("mileage").notNullable();
-      table.text("transmission type");
-      table.boolean("clean");
+  return knex.schema.createTable("cars", table => {
+    table.increments();
+    table
+      .text("VIN", 128)
+      .unique()
+      .notNullable();
+    table.text("make").notNullable();
+    table.text("model").notNullable();
+    table.integer("mileage").notNullable();
+    table.text("transmission type");
+    table.boolean("clean");
   });
 };
 
--- data/seeds/01-add-cars.js
+++ data/seeds/01-add-cars.js
@@ -1,13 +1,31 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('cars').truncate()
-    .then(function () {
+  return knex("cars")
+    .truncate()
+    .then(function() {
       // Inserts seed entries
-      return knex('cars').insert([
-        {VIN: "test123", make: "Audi", model: "A6", mileage: 120000, clean: true},
-        {VIN: "test456", make: "Huyndai", model: "Getz 1.4", mileage: 75000, clean: false},
-        {VIN: "test789", make: "Ford Mustang", model: "GT500", mileage: 25000, clean: true}
+      return knex("cars").insert([
+        {
+          VIN: "test123",
+          make: "Audi",
+          model: "A6",
+          mileage: 120000,
+          clean: true
+        },
+        {
+          VIN: "test456",
+          make: "Huyndai",
+          model: "Getz 1.4",
+          mileage: 75000,
+          clean: false
+        },
+        {
+          VIN: "test789",
+          make: "Ford Mustang",
+          model: "GT500",
+          mileage: 25000,
+          clean: true
+        }
       ]);
     });
 };
